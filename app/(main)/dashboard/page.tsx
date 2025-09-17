936b1b12ede358bcf1ca0e27f0e5d28220f8d10e
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-17 18:17:51 +0530

    add custom hooks

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useConvexQuery } from "@/hooks/useConvex";
+
 export default function Dashboard() {
+  const { data: projects, isLoading } = useConvexQuery(
+    api.projects.getCurrentProject,
+  );
+
+  console.log(projects);
   return <div>Dashboard</div>;
 }
