7698d7510ea5cc4b7c10001d4b838c24ebbd577b
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-29 22:08:55 +0530

    add v1 typing logic

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -1,8 +1,40 @@
-export default function TextInput() {
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
+
+export default function TextInput({ text }: { text: string }) {
+  const [typed, setTyped] = useState('');
+  const [index, setIndex] = useState(0);
+
+  const { words, totalWords } = useMemo(() => {
+    const words = text.split(' ');
+    const totalWords = words.length;
+    return { words, totalWords };
+  }, [text]);
+
+  useEffect(() => {
+    const currentWord = words[index];
+
+    if (typed === ' ') {
+      setTyped('');
+      return;
+    }
+
+    if (typed[currentWord.length] === ' ') {
+      setTyped('');
+      setIndex((prev) => prev + 1);
+      return;
+    }
+  }, [typed]);
+
   return (
     <div>
       <input
         type="text"
+        value={typed}
+        onChange={(e) => setTyped(e.target.value)}
+        autoFocus
+        onPaste={(e) => e.preventDefault()}
         className="w-full rounded-lg border border-neutral-300 p-1 focus:border-black focus:outline-none"
       />
     </div>
--- app/components/TypingArea.tsx
+++ app/components/TypingArea.tsx
@@ -1,12 +1,15 @@
 import TextInput from './TextInput';
 
 export default function TypingArea() {
+  const text = 'Here goes the text to be typed';
   return (
     <div className="w-full space-y-4 rounded-lg border border-neutral-300 bg-neutral-100 px-4 py-5 font-mono font-light">
-      <div>Here goes the text to be typed</div>
+      <div>
+        <p>{text}</p>
+      </div>
 
       <div>
-        <TextInput />
+        <TextInput text={text} />
       </div>
     </div>
   );
