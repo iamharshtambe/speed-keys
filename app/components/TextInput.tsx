0e35b708425b7db70c85b15ccf6592b2cef16fca
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-30 18:56:07 +0530

    use typing logic hook

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -1,60 +1,9 @@
 'use client';
 
-import { useEffect, useMemo, useState } from 'react';
-
-type TypingError = { state: false } | { state: true; at: number };
+import { useTypingLogic } from '../hooks/useTypingLogic';
 
 export default function TextInput({ text }: { text: string }) {
-  const [typed, setTyped] = useState('');
-  const [index, setIndex] = useState(0);
-  const [mismatch, setMismatch] = useState<TypingError>({ state: false });
-
-  const { words, totalWords } = useMemo(() => {
-    const words = text.split(' ');
-    return { words, totalWords: words.length };
-  }, [text]);
-
-  useEffect(() => {
-    if (index >= totalWords) return; // stop if already past last word
-
-    const currentWord = words[index];
-    const typedLength = typed.length;
-
-    // prevent leading space
-    if (typed === ' ') {
-      setTyped('');
-      return;
-    }
-
-    // mismatch reset
-    if (mismatch.state) {
-      if (mismatch.at >= typedLength) {
-        setMismatch({ state: false });
-      }
-      return;
-    }
-
-    // check if the last word is completely typed
-    if (index === totalWords - 1) {
-      if (typed === currentWord) {
-        return;
-      }
-    }
-
-    // move to next word when space typed correctly
-    if (typed[currentWord.length] === ' ') {
-      if (typed.slice(0, -1) === currentWord) {
-        setTyped('');
-        setIndex((prev) => prev + 1);
-      }
-      return;
-    }
-
-    // mismatch detection
-    if (typed[typedLength - 1] !== currentWord[typedLength - 1]) {
-      setMismatch({ state: true, at: typedLength - 1 });
-    }
-  }, [typed, index, words, totalWords, mismatch]);
+  const { typed, setTyped, raceOver } = useTypingLogic({ text });
 
   return (
     <div>
@@ -64,6 +13,7 @@ export default function TextInput({ text }: { text: string }) {
         onChange={(e) => setTyped(e.target.value)}
         autoFocus
         onPaste={(e) => e.preventDefault()}
+        disabled={raceOver}
         className="w-full rounded-lg border border-neutral-300 p-1 focus:border-black focus:outline-none"
       />
     </div>
