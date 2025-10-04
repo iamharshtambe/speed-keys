7a3e5e759931da998e85a4513b1c520816cb4cd6
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-04 19:31:18 +0530

    restart

--- app/components/RaceTrack.tsx
+++
@@ -1,3 +0,0 @@
-export default function RaceTrack() {
-  return <div>RaceTrack</div>;
-}
--- lib/getTypingText.ts
+++
@@ -1,8 +0,0 @@
-import { typingTexts } from '@/data/data';
-
-export function getTypingText() {
-  return typingTexts[Math.floor(Math.random() * typingTexts.length)]
-    .split(' ')
-    .slice(0, 5)
-    .join(' ');
-}
